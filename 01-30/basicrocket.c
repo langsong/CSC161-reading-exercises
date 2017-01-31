b889976a49ed4ced2a7d01b6f3283681da68716e
Lang Song <songlang@cs.grinnell.edu>
2017-01-31 12:19:22 -0600

    Reading exercise

---
+++ 01-30/basicrocket.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void printCone(void) {
+  printf("    /\\\n");
+  printf("   /  \\\n");
+  printf("  /    \\\n");
+  printf("  ------\n");
+}
+
+void printBox(void) {
+  printf("  ------\n");
+  printf("  |    |\n");
+  printf("  |    |\n");
+  printf("  ------\n");
+}
+
+void drawRocketShip(void) {
+  printCone();
+  printBox();
+  printCone();
+}
+
+int main(void) {
+  drawRocketShip();
+}
+
