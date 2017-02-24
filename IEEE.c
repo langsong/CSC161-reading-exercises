db73a65bbb5d03d9b402c7b98e30ae12516211cd
Lang Song <songlang@cs.grinnell.edu>
2017-02-24 14:08:57 -0600

    "IEEE homework"

---
+++ IEEE.c
@@ -0,0 +1,8 @@
+/*
+
+first number 0 indicates it's positive;
+then the next eight digits 10000101 gives exponent 133-127=6;
+the rest of the digits give 1 adding one half, one eighth, one sixteenth, and one one hundred twenty-eighth, which is 217/128;
+so the number represented is 2^6 times 217/128 = 217/2
+
+*/
